af3e03977c66e0ca8c996a8db4d300b848742a13
Geovanny Mina <geodmina@gmail.com>
2017-09-11 12:10:59 -0500

    Update UserTest.php

--- UserTest.php
+++ UserTest.php
@@ -10,7 +10,7 @@ class UserTest extends TestCase{
 
   public function testTalk() {
     $user = new User();
-    $expected = "Hello world!35";
+    $expected = "Hello world!";
     $actual = $user->talk();
     $this->assertEquals($expected, $actual);
   }
